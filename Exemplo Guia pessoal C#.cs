d74b39b3d241bdcb13286549265eaf840109f872
Ezequiel Bitencourt <ezequiel.bitencourt197@gmail.com>
2021-11-06 14:54:09 -0300

    Exemplo do guia peossal

---
+++ Exemplo Guia pessoal C#.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este é meu Guia pessoal de Git/GitHub");
+        }
+    }
+}
